030cd0ba651e2f0c14405230733b73be3e99de0a
pakitoslash <fiunam94@gmail.com>
2014-10-14 12:48:40 -0500

    Archiv cuando el usuario no metio los datos correctos

---
+++ Validadcion_Pagina/inbad.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Datos Incorrectos</title>
+</head>
+<body>
+		<link rel="stylesheet" href="css/micss.css">
+
+		<?php
+		include("auxiliar.php")  //Inclute el diseño de otras paginas
+		?>
+
+		<h1 align="center">"REVISA QUE TUS DATOS ESTEN CORRECTOS"</h1>
+</body>
+</html>
