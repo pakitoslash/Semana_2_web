bf4c3cad04898c07ba9f59e73f26e9a579b63b96
pakitoslash <fiunam94@gmail.com>
2014-10-14 12:58:39 -0500

    Agregando archivo cuando el usuario existe

---
+++ Validadcion_Pagina/inok.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>BIENVENIDO</title>
+</head>
+<body>
+	<link rel="stylesheet" href="css/micss.css">
+
+	<?php
+		include("auxiliar.php")  //Inclute el diseño de otras paginas
+	?>
+
+	<h1 align="center">BIENVENIDO A TU PAGINA</h1>
+</body>
+</html>
