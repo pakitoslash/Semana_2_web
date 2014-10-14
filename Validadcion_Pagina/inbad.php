e1c17ecbf12cf0fcea34dca8e25ba40c5b128e83
pakitoslash <fiunam94@gmail.com>
2014-10-14 12:31:55 -0500

    Mir archivos

---
+++ Validadcion_Pagina/inbad.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Datos Incorrectos</title>
+</head>
+<body>
+		<h1 align="center">"REVISA QUE TUS DATOS ESTEN CORRECTOS"</h1>
+</body>
+</html>
---
+++ Validadcion_Pagina/inok.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>BIENVENIDO</title>
+</head>
+<body>
+	<h1 align="center">BIENVENIDO A TU PAGINA</h1>
+</body>
+</html>
