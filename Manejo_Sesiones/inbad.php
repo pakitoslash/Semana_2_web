1c5fb9a960caa5aa773a36cdee4f32ca10f6376c
pakitoslash <fiunam94@gmail.com>
2014-10-14 13:02:17 -0500

    Todos los archivos

--- Manejo_Sesiones/inbad.php
+++ Manejo_Sesiones/inbad.php
@@ -4,6 +4,12 @@
 	<title>Datos Incorrectos</title>
 </head>
 <body>
+		<link rel="stylesheet" href="css/micss.css">
+
+		<?php
+		include("auxiliar.php")  //Inclute el diseño de otras paginas
+		?>
+
 		<h1 align="center">"REVISA QUE TUS DATOS ESTEN CORRECTOS"</h1>
 </body>
 </html>
--- Manejo_Sesiones/inok.php
+++ Manejo_Sesiones/inok.php
@@ -4,6 +4,12 @@
 	<title>BIENVENIDO</title>
 </head>
 <body>
+	<link rel="stylesheet" href="css/micss.css">
+
+	<?php
+		include("auxiliar.php")  //Inclute el diseño de otras paginas
+	?>
+
 	<h1 align="center">BIENVENIDO A TU PAGINA</h1>
 </body>
 </html>
